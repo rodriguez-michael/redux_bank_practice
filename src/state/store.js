3ab8e573b4dd405c73a21c28b336c1245234a5ba
rodriguez-michael <github@michaelrodriguez.dev>
2022-03-06 18:33:41 -0600

    useSelctor utilized to be able to retrieve state in any component

--- src/state/store.js
+++ src/state/store.js
@@ -1,10 +1,11 @@
 import { createStore } from "redux";
 import reducers from "./reducers/index";
 
-const store = createStore(
+export const store = createStore(
   reducers,
   // default state
   {}
 )
 
-export default store;
+//store returns key value pairs from combined reducers (reducers index.js)
+//combnined reducers takes in all individual reducers
